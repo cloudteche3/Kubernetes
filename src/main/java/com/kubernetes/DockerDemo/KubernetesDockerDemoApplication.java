35e109c8d976d876e95ef6dfaecfc3e557e0438e
Ankit <cloudteche3@gmail.com>
2021-10-15 01:03:26 +0530

    removed unused comment

--- src/main/java/com/kubernetes/DockerDemo/KubernetesDockerDemoApplication.java
+++ src/main/java/com/kubernetes/DockerDemo/KubernetesDockerDemoApplication.java
@@ -11,15 +11,7 @@ public class KubernetesDockerDemoApplication {
 		SpringApplication.run(KubernetesDockerDemoApplication.class, args);
 		
 
-		System.out.println("this is my testing branch");
-		System.out.println("this is my testing branch ankit");
-		System.out.println("this is my testing branch1 pravitha");
-		System.out.println("this is my testing branch2 meenu");
-		System.out.println("this is my testing branc3 rahul ");
-		System.out.println("this is my testing branc4 niji");
-				System.out.println("this is my testing branch1 pravitha");
-		System.out.println("this is my testing branch1 pravitha");
-		System.out.println("this is my testing branch1 pravitha");
+
 
 		
 	
