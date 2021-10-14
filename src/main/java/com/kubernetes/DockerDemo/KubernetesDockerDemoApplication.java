23d5d0811dac9fd9b19fa3f8c7199e9eb1cfef04
Ankit <cloudteche3@gmail.com>
2021-10-14 21:06:54 +0530

    hello

---
+++ src/main/java/com/kubernetes/DockerDemo/KubernetesDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kubernetes.DockerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesDockerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubernetes/DockerDemo/KubernetesDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubernetes.DockerDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
